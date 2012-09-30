52a7b1e5c64bec9587255bb4c87523f7fe214523
aalmiray <aalmiray@gmail.com>
2012-09-30 11:36:13 -0700

    3. add a menuBar and action buttons

--- griffon-app/views/addressbook/AddressbookView.groovy
+++ griffon-app/views/addressbook/AddressbookView.groovy
@@ -8,15 +8,26 @@ application(title: 'Addressbook',
   iconImages: [imageIcon('/griffon-icon-48x48.png').image,
                imageIcon('/griffon-icon-32x32.png').image,
                imageIcon('/griffon-icon-16x16.png').image]) {
+    menuBar {
+        menu('Contacts') {
+            menuItem('New')
+            menuItem('Save')
+            menuItem('Delete')
+            menuItem('Dump')
+        }
+    }
     migLayout(layoutConstraints: 'fill')
     scrollPane(constraints: 'west, w 180!', border: titledBorder('Contacts')) {
         list(id: 'contactList')
     }
     panel(constraints: 'center, grow', border: titledBorder(title: 'Contact')) {
-        label 'Contact form'
+        label 'Empty (for now)'
     }
     panel(constraints: 'east, grow', border: titledBorder(title: 'Actions')) {
         migLayout(layoutConstraints: 'fill, wrap')
-        label 'Buttons'
+        button('New')
+        button('Save')
+        button('Delete')
+        button('Dump')
     }
 }
