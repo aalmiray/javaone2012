0916b8ddea617d833b9465c77a344bb5032009d0
aalmiray <aalmiray@gmail.com>
2012-09-30 11:34:34 -0700

    2. apply main layout

--- griffon-app/views/addressbook/AddressbookView.groovy
+++ griffon-app/views/addressbook/AddressbookView.groovy
@@ -1,14 +1,22 @@
 package addressbook
 
-application(title: 'addressbook',
-  preferredSize: [320, 240],
+application(title: 'Addressbook',
   pack: true,
-  //location: [50,50],
+  resizable: true,
   locationByPlatform:true,
   iconImage: imageIcon('/griffon-icon-48x48.png').image,
   iconImages: [imageIcon('/griffon-icon-48x48.png').image,
                imageIcon('/griffon-icon-32x32.png').image,
                imageIcon('/griffon-icon-16x16.png').image]) {
-    // add content here
-    label('Content Goes Here') // delete me
+    migLayout(layoutConstraints: 'fill')
+    scrollPane(constraints: 'west, w 180!', border: titledBorder('Contacts')) {
+        list(id: 'contactList')
+    }
+    panel(constraints: 'center, grow', border: titledBorder(title: 'Contact')) {
+        label 'Contact form'
+    }
+    panel(constraints: 'east, grow', border: titledBorder(title: 'Actions')) {
+        migLayout(layoutConstraints: 'fill, wrap')
+        label 'Buttons'
+    }
 }
